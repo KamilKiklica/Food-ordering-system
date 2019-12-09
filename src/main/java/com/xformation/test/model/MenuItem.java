522c2b6cc10888e905ffe65c679e6d6f3c67f127
kamkik <kamil.kiklica@gmail.com>
2019-12-09 14:37:42 +0100

    Added Template for Menu Items

---
+++ src/main/java/com/xformation/test/model/MenuItem.java
@@ -0,0 +1,7 @@
+package com.xformation.test.model;
+
+public class MenuItem extends MenuItemTemplate {
+    public MenuItem(String nameOfDish, int priceOfDish) {
+        super(nameOfDish,priceOfDish);
+    }
+}
